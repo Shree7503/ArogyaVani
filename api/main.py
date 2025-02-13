48443d70f6131b42d328b36f60c5d5990f5a704a
Shreenath Kadam <146519199+Shree7503@users.noreply.github.com>
2025-02-14 00:59:06 +0530

    added requirements

--- api/main.py
+++ api/main.py
@@ -1,11 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
-
+from pathlib import Path
 
 app = FastAPI()
 
+# Set up Jinja2 template directory
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def main():
-    return templates.TemplateResponse("main.html")
+async def index(request: Request):
+    return templates.TemplateResponse("main.html", {"request": request})
